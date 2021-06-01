8d8d66785b26396f407e039efc96b3d0157cad05
Alfonso Cerv <pedrocerv1@hotmail.com>
2021-06-01 13:09:00 -0500

    added javascript and jquery to handle task management

---
+++ assets/script.js
@@ -0,0 +1,38 @@
+var tasks = $('.description');
+var savedTasks = JSON.parse(localStorage.getItem("savedTasks")) || [];
+
+function init() {
+  var currentDate = moment();
+
+  $("#currentDay").html(currentDate.format("dddd, MMMM DD"));
+  
+  var currentHour = currentDate.hours();
+  
+  $(".description").addClass("future");
+  
+  if(currentHour > 17){
+    $(".description").removeClass("future");
+    $(".description").addClass("past");
+  } else {
+  for(var i = currentHour - 1; i >= 9; i--){
+    $(".description").eq(i - 9).removeClass("future");
+    $(".description").eq(i - 9).addClass("past");
+    }
+    $(".description").eq(currentHour - 9).removeClass("future");
+    $(".description").eq(currentHour - 9).addClass("present");
+  }
+  loadTasks();
+};
+
+init();
+
+function saveTask(index) {
+  savedTasks[index] = tasks[index].value;
+  localStorage.setItem("savedTasks", JSON.stringify(savedTasks));
+};
+
+function loadTasks() {
+  for(var i = 0; i < savedTasks.length; i++) {
+    tasks[i].value = savedTasks[i];
+  }
+};
